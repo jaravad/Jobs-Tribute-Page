78d9f169da9c0279f9fd4f04be21e539da0434a2
jaravad <jaravad@uninorte.edu.co>
2020-06-28 22:32:44 -0500

    some visual improvements, animations added

--- script.js
+++ script.js
@@ -1,17 +1,19 @@
+AOS.init({ duration: 600, easing: 'ease-in-sine', delay: 100 });
+
 const quotes = [
-  "Stay hungry. Stay foolish.",
-  "Your time is limited, so don’t waste it living someone else’s life.",
-  "Don’t let the noise of others’ opinions drown out your own inner voice.",
-  "Have the courage to follow your heart and intuition. They somehow already know what you truly want to become.",
-  "If today were the last day of your life, would you want to do what you are about to do today?",
-  "I think the things you regret most in life are the things you didn’t do.",
-  "Remembering that you are going to die is the best way I know to avoid the trap of thinking you have something to lose.",
-  "We’re here to put a dent in the universe. Otherwise why else even be here?",
-  "My favorite things in life don’t cost any money. It’s really clear that the most precious resource we all have is time.",
-  "Your work is going to fill a large part of your life, and the only way to be truly satisfied is to do what you believe is great work. And the only way to do great work is to love what you do. If you haven’t found it yet, keep looking. Don’t settle. As with all matters of the heart, you’ll know when you find it.",
-  "You can’t connect the dots looking forward; you can only connect them looking backward. So you have to trust that the dots will somehow connect in your future."
+  'Stay hungry. Stay foolish.',
+  'Your time is limited, so don’t waste it living someone else’s life.',
+  'Don’t let the noise of others’ opinions drown out your own inner voice.',
+  'Have the courage to follow your heart and intuition. They somehow already know what you truly want to become.',
+  'If today were the last day of your life, would you want to do what you are about to do today?',
+  'I think the things you regret most in life are the things you didn’t do.',
+  'Remembering that you are going to die is the best way I know to avoid the trap of thinking you have something to lose.',
+  'We’re here to put a dent in the universe. Otherwise why else even be here?',
+  'My favorite things in life don’t cost any money. It’s really clear that the most precious resource we all have is time.',
+  'Your work is going to fill a large part of your life, and the only way to be truly satisfied is to do what you believe is great work. And the only way to do great work is to love what you do. If you haven’t found it yet, keep looking. Don’t settle. As with all matters of the heart, you’ll know when you find it.',
+  'You can’t connect the dots looking forward; you can only connect them looking backward. So you have to trust that the dots will somehow connect in your future.',
 ];
 
 let i = Math.floor(Math.random() * quotes.length);
-const h2 = document.getElementById("quote");
+const h2 = document.getElementById('quote');
 h2.innerText = quotes[i];
